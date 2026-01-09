eba5792114928f6dba1482f6d19e7c9be124c9fe
yahelmakbili <yahel.makbili@post.runi.ac.il>
2026-01-09 19:51:25 +0200

    new

--- Network.java
+++ Network.java
@@ -148,13 +148,18 @@ public class Network {
     // Returns a textual description of all the users in this network, and who they
     // follow.
     public String toString() {
-        String ans = "Network:\n";
-        for (int i = 0; i < userCount; i++) {
-            ans += users[i].toString();
-            if (i < userCount - 1) {
-                ans += "\n";
+        String ans = "Network:";
+
+        if (userCount > 0) {
+            ans += "\n";
+            for (int i = 0; i < userCount; i++) {
+                ans += users[i].toString();
+                if (i < userCount - 1) {
+                    ans += "\n";
+                }
             }
         }
+
         return ans;
     }
 }
